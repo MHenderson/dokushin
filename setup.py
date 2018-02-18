102515936b33aeb80d60dd223f965484470f0b72
Matthew Henderson <matthew.james.henderson@gmail.com>
2018-02-18 10:26:04 +0000

    Remove all but the Python package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sudoku',
+      version=0.1,
+      description="",
+      url="https://github.com/MHenderson/sudoku",
+      author="Matthew Henderson",
+      author_email="matthew.james.henderson@gmail.com",
+      license="MIT",
+      packages=['sudoku'],
+      zip_safe=False)
