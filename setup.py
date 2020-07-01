69d666b635ad7b007013e59892d96abfdb1afea6
Matthew Henderson <matthew.james.henderson@gmail.com>
2020-07-01 14:33:21 +0100

    rename package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='sudoku',
+setup(name='sussudoku',
       version=0.2,
       description="Solve Sudoku with Python.",
       url="https://gitlab.com/MHenderson1/sudoku",
       author="Matthew Henderson",
       author_email="matthew.james.henderson@gmail.com",
       license="MIT",
-      packages=['sudoku'],
+      packages=['sussudoku'],
       zip_safe=False)
