a69c4de4af3822774b879c76238a24f3d89fee68
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-05-01 09:43:02 +0000

    Resolve "Rename: dokushin"

--- sudoku_test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import sudoku
-
-def test_0():
-    assert len(sudoku.cells(0))==len([])
-    assert len(sudoku.symbols(0))==len([])
-    assert len(sudoku.cells_by_row(0))==len([])
-
